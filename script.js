2df7667fcd5aca05054f318b4eff23886c1abc1c
Nathália <nathaliamarciaandrade@gmail.com>
2022-09-13 18:16:59 -0300

    Realizando o requisito 5

--- script.js
+++ script.js
@@ -77,7 +77,9 @@ const createCartItemElement = ({ id, title, price }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `ID: ${id} | TITLE: ${title} | PRICE: $${price}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', () => {
+    li.remove();
+  });
   return li;
 };
 
